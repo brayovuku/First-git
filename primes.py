1993193a81e0c48a45154830ce4525c72b934add
brayovuku <34342035+brayovuku@users.noreply.github.com>
2019-02-06 21:49:11 +0300

    Create primes.py

---
+++ primes.py
@@ -0,0 +1,25 @@
+import math
+max=int(input())
+if max>6000:
+   max=6000
+
+primes=[2,3,5] # pre-stored coz of the unique 6k+i formulae
+k=1
+i=1
+x=(6*k)+i
+while x<=max:
+    x=(6*k)+i
+    flag=False
+    for y in range(5,(math.ceil(math.sqrt(x)))+1,+2):
+        if x%y==0:
+            flag=True
+            break
+    if i==1:
+        i=5
+    else:
+        i=1;
+        k=k+1
+    if flag==False:
+        if x<=max:
+            primes.append(x)
+print (primes)
